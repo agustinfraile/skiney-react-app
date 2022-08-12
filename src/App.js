662a373a9baa58fb0e275e08795407bb9895aa9c
Agustin Fraile <agustinfraile98@gmail.com>
2022-08-12 18:25:30 -0300

    creando componente navbar

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Aprende React!
+        </a>    
       </header>
     </div>
   );
